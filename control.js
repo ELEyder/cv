203a0cbbd16265683ffe1a3d84426452881ac29f
Eyder <74992528+ELEyder@users.noreply.github.com>
2023-06-14 22:35:15 -0500

    Add files via upload

---
+++ control.js
@@ -0,0 +1,6 @@
+function mostrarPerfil(){
+    document.getElementById("perfil").style.display = "block";
+    alert("XD");
+    document.getElementById("historial").style.display = "none";
+    alert("XD");
+}
