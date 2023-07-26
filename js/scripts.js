ecf35e09cb0a2cf173906cb1b9353a0c6505bb4f
eyder <74992528+ELEyder@users.noreply.github.com>
2023-07-26 10:22:32 -0500

    Github contacto Agregado

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,37 @@
-let contador = 0;
-var largo = 0;
-const imagenes = ["img/miCara.png" , "img/miCara2.png"];
-largo = imagenes.length;
-function cambiarImagen() {
-    var fotoInicial = document.getElementById('miFoto');
-    contador++;
-    if (contador == largo){
-        contador = 0;
-    }
-    fotoInicial.style.opacity = 0;
-    setTimeout(function(){
-        fotoInicial.src = imagenes[contador];
-        fotoInicial.style.opacity = 1;
-        
-    }, 500);
+// Clase para las imagenes y contador
+class imagenes{
+    constructor(...args){
+        this.contador = 0;
+        this.imagenes = args;
+    }  
+}
+// Objetos
+var img1 = new imagenes("img/miCara.png" , "img/miCara2.png");
+var img2 = new imagenes("img/senati.png" , "img/senati2.png");
 
+// Funcion de cambiar las imagenes
+function cambiarImagen(id) {
+    var fotoInicial = document.getElementById(id);
+    if (id == "miFoto"){
+        img1.contador++;
+        if (img1.contador == img1.imagenes.length){
+            img1.contador = 0;
+        }
+        fotoInicial.style.opacity = 0;
+        setTimeout(function(){
+            fotoInicial.src = img1.imagenes[img1.contador];
+            fotoInicial.style.opacity = 1;
+        }, 500);
+    } else {
+        img2.contador++;
+        if (img2.contador == img2.imagenes.length){
+            img2.contador = 0;
+        }
+        fotoInicial.style.opacity = 0;
+        setTimeout(function(){
+            fotoInicial.src = img2.imagenes[img2.contador];
+            fotoInicial.style.opacity = 1;
+        }, 500);
+    }
+    
 };
