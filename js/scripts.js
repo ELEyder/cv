dbe7fa8179c5cc2107994b06ebe9f22c55a0c197
eyder <74992528+ELEyder@users.noreply.github.com>
2023-07-29 14:38:45 -0500

    Agregar otra foto a senati y mejorar funcion de js

--- js/scripts.js
+++ js/scripts.js
@@ -7,31 +7,23 @@ class imagenes{
 }
 // Objetos
 var img1 = new imagenes("img/miCara.png" , "img/miCara2.png");
-var img2 = new imagenes("img/senati.png" , "img/senati2.png");
+var img2 = new imagenes("img/senati.png" , "img/senati2.png" , "img/senati3.png");
 
 // Funcion de cambiar las imagenes
 function cambiarImagen(id) {
     var fotoInicial = document.getElementById(id);
     if (id == "miFoto"){
-        img1.contador++;
-        if (img1.contador == img1.imagenes.length){
-            img1.contador = 0;
-        }
-        fotoInicial.style.opacity = 0;
-        setTimeout(function(){
-            fotoInicial.src = img1.imagenes[img1.contador];
-            fotoInicial.style.opacity = 1;
-        }, 500);
+        var imgFinal = img1
     } else {
-        img2.contador++;
-        if (img2.contador == img2.imagenes.length){
-            img2.contador = 0;
-        }
-        fotoInicial.style.opacity = 0;
-        setTimeout(function(){
-            fotoInicial.src = img2.imagenes[img2.contador];
-            fotoInicial.style.opacity = 1;
-        }, 500);
+        var imgFinal = img2
     }
-    
+    imgFinal.contador++;
+    if (imgFinal.contador == imgFinal.imagenes.length){
+        imgFinal.contador = 0;
+    }
+    fotoInicial.style.opacity = 0;
+    setTimeout(function(){
+        fotoInicial.src = imgFinal.imagenes[imgFinal.contador];
+        fotoInicial.style.opacity = 1;
+    }, 500);
 };
