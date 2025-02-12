a87152d4b0f9f32b83039b01f825a73e59c355ea
ELEyder <74992528+ELEyder@users.noreply.github.com>
2025-02-12 15:57:23 -0500

    Detaller menores corregidos

--- src/views/Contacts/index.tsx
+++ src/views/Contacts/index.tsx
@@ -3,7 +3,6 @@ import styles from "./index.module.css";
 import { ReactComponent as GitHub } from '../../assets/contact/github.svg';
 import { ReactComponent as LinkedIn } from '../../assets/contact/linkedin.svg';
 import { ReactComponent as Correo } from '../../assets/contact/email.svg';
-import { ReactComponent as Internet } from '../../assets/contact/internet.svg';
 import { ReactComponent as Whatsapp } from '../../assets/contact/whatsapp.svg';
 import { ReactComponent as Phone } from '../../assets/contact/phone.svg';
 
