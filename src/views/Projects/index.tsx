52d1c9e05b71bbf1db8c9cfb06385b2426f3cdaa
ELEyder <74992528+ELEyder@users.noreply.github.com>
2025-02-11 20:28:21 -0500

    Tecnologías añadidas

--- src/views/Projects/index.tsx
+++ src/views/Projects/index.tsx
@@ -1,5 +1,5 @@
 import styles from "./index.module.css";
-import { Image } from "antd";
+import { Image, Tooltip } from "antd";
 
 function Projects() {
   const projects = [
@@ -8,8 +8,9 @@ function Projects() {
       name: "Portafolio",
       github: "https://github.com/ELEyder/cv",
       deploy: "https://eleyder.github.io/cv/",
-      description: "ERP para una empresa textil",
-      tech: [
+      description:
+        "Mi protafolio personal diseñado y desarrollado para mostrar mis proyectos personales y profesionales.",
+      techs: [
         {
           name: "React",
           icon: "./img/tech/react.svg",
@@ -22,7 +23,41 @@ function Projects() {
       github: "https://github.com/ELEyder/kaliffo-erp",
       deploys: "https://eleyder.github.io/cv/",
       description: "ERP para una empresa textil",
-      tech: [
+      techs: [
+        {
+          name: "React",
+          icon: "./img/tech/react.svg",
+        },
+        {
+          name: "Node Js",
+          icon: "./img/tech/node.svg",
+        },
+      ],
+    },
+    {
+      img: "p3",
+      name: "CinesCat",
+      github: "https://github.com/ELEyder/kaliffo-erp",
+      deploys: "https://eleyder.github.io/cv/",
+      description: "ERP para una empresa textil",
+      techs: [
+        {
+          name: "React",
+          icon: "./img/tech/react.svg",
+        },
+        {
+          name: "Spring Boot",
+          icon: "./img/tech/spring.svg",
+        },
+      ],
+    },
+    {
+      img: "p2",
+      name: "Kaliffo-ERP",
+      github: "https://github.com/ELEyder/kaliffo-erp",
+      deploys: "https://eleyder.github.io/cv/",
+      description: "ERP para una empresa textil",
+      techs: [
         {
           name: "React",
           icon: "./img/tech/react.svg",
@@ -35,33 +70,7 @@ function Projects() {
       github: "https://github.com/ELEyder/kaliffo-erp",
       deploys: "https://eleyder.github.io/cv/",
       description: "ERP para una empresa textil",
-      tech: [
-        {
-          name: "React",
-          icon: "./img/tech/react.svg",
-        },
-      ],
-    },
-    {
-      img: "p2",
-      name: "Kaliffo-ERP",
-      github: "https://github.com/ELEyder/kaliffo-erp",
-      deploys: "https://eleyder.github.io/cv/",
-      description: "ERP para una empresa textil",
-      tech: [
-        {
-          name: "React",
-          icon: "./img/tech/react.svg",
-        },
-      ],
-    },
-    {
-      img: "p2",
-      name: "Kaliffo-ERP",
-      github: "https://github.com/ELEyder/kaliffo-erp",
-      deploys: "https://eleyder.github.io/cv/",
-      description: "ERP para una empresa textil",
-      tech: [
+      techs: [
         {
           name: "React",
           icon: "./img/tech/react.svg",
@@ -86,8 +95,17 @@ function Projects() {
                   width="100%"
                 />
                 <div className={styles.text}>
-                <h2>{project.name}</h2>
-                <p>{project.description}</p>
+                  <h2>{project.name}</h2>
+                  <p>{project.description}</p>
+                  <div className={styles.techs}>
+                    {project.techs.map((tech) => {
+                      return <div className={styles.tech}>
+                        <Tooltip title={tech.name}>
+                          <img src={tech.icon} alt={tech.name} />
+                        </Tooltip>
+                      </div>;
+                    })}
+                  </div>
                 </div>
               </div>
             );
