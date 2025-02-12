916d076ed869b5383476300f8fd559deb8535669
ELEyder <74992528+ELEyder@users.noreply.github.com>
2025-02-12 18:20:37 -0500

    Error de tooltip

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -11,12 +11,10 @@ const Menu: React.FC<MenuProps> = ({ items }) => {
   return (
     <nav className={styles.menu}>
       {items.map((item, index) => (
-        <Tooltip title={item.label} placement="right">
           <Link key={index} duration={100} to={item.href} className={styles.link}>
             {item.icon}
             <span>{item.label}</span>
           </Link>
-        </Tooltip>
       ))}
     </nav>
   );
