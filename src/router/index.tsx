3a58d71fc5166cb690292282dda78aa3883dd8a1
ELEyder <74992528+ELEyder@users.noreply.github.com>
2025-02-19 18:51:22 -0500

    Páginas optimizadas con Lazy

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,23 +1,24 @@
+import { Suspense } from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "../layouts/Layout";
-import Home from "../views/Home";
-import Error from "../views/Error";
-import Blog from "../views/Blog";
-import BlogDetail from "../views/BlogDetail";
+import { Home, Blog, BlogDetail, Error } from "../views";
+import { Loading } from "../components";
 
 const Routing = () => {
   return (
     <HashRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:id" element={<BlogDetail />} />
-        </Route>
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:id" element={<BlogDetail />} />
+          </Route>
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Suspense>
     </HashRouter>
   );
-}
+};
 
-export default Routing
+export default Routing;
---
+++ src/views/index.tsx
@@ -0,0 +1,6 @@
+import { lazy } from "react";
+
+export const  Home = lazy(() => import("./Home"));
+export const  Blog = lazy(() => import("./Blog"));
+export const  BlogDetail = lazy(() => import("./BlogDetail"));
+export const  Error = lazy(() => import("./Error"));
