92f5b5d56257edf6c78f06aa3b452b63c680d8ef
ELEyder <74992528+ELEyder@users.noreply.github.com>
2025-02-19 01:14:04 -0500

    vite config actualizado

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from '@svgr/rollup';
 
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/cv/",
+  base: isGitHubPages ? '/cv/' : './',
   plugins: [react(), svgr()],
 })
