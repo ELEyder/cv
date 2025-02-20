8126894302142459ffe335ac3d7e740be338246c
ELEyder <74992528+ELEyder@users.noreply.github.com>
2025-02-19 19:44:37 -0500

    Nuevos env

---
+++ src/services/firestore/app.tsx
@@ -0,0 +1,21 @@
+// src/services/firestore/app.tsx
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+// Configuración de Firebase usando variables del entorno
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+// Inicializar Firebase
+export const app = initializeApp(firebaseConfig);
+
+// Inicializar Firestore
+const db = getFirestore(app);
+
+export { db };
