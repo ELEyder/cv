103791a7591cd470b47311b54d1bd8f4f5b2247a
ELEyder <74992528+ELEyder@users.noreply.github.com>
2025-02-28 08:48:12 -0500

    change cv

--- src/sections/AboutMe/index.tsx
+++ src/sections/AboutMe/index.tsx
@@ -3,21 +3,30 @@ import Button from "../../components/Button";
 
 const AboutMe = () => {
   return (
-        <section className={styles.aboutMe} id="aboutMe">
-          <div className={styles.text}>
-            <div className={styles.content}>
-            <h1>Hola, Soy Eyder - Desarrollador Full Stack</h1>
-            <p>Desarrollador junior enfocado en crear aplicaciones web con React. Con experiencia en APIs REST y Java Spring Boot, me apasiona aprender y mejorar continuamente mis habilidades en desarrollo web.</p>
-            <Button onClick={() => {
-              window.open('pdf/Eyder_Huayta_CV.pdf', '_blank')
-            }}>Descargar CV</Button>
-            </div>
-          </div>
-          <div className={styles.media}>
-            <img className={styles.banner} src={"./images/bg/bg1.webp"} alt="bg1" />
-          </div>
-        </section>
+    <section className={styles.aboutMe} id="aboutMe">
+      <div className={styles.text}>
+        <div className={styles.content}>
+          <h1>Hola, Soy Eyder - Desarrollador Web</h1>
+          <p>
+            Desarrollador de Software con 2 años de experiencia en desarrollo
+            web. Sólidos conocimientos en diseño, implementación y mantenimiento           de soluciones de software, con habilidades en Php, Java y
+            JavaScript, y experiencia en tecnologías como Laravel, Spring Boot y
+            React
+          </p>
+          <Button
+            onClick={() => {
+              window.open("pdf/Eyder_Huayta_CV.pdf", "_blank");
+            }}
+          >
+            Descargar CV
+          </Button>
+        </div>
+      </div>
+      <div className={styles.media}>
+        <img className={styles.banner} src={"./images/bg/bg1.webp"} alt="bg1" />
+      </div>
+    </section>
   );
-}
+};
 
 export default AboutMe;
